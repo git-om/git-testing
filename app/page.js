3a1a26ac6b7f60e5af7d579fe944a61b3353c3c7
Om Patel <137379875+git-om@users.noreply.github.com>
2025-02-10 16:15:11 +0530

    Add click handler to header for play functionality

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
+  function player() {
+    console.log("play");
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>This is git file</h1>
+      <h1 onClick={player}>This is git file</h1>
     </div>
   );
 }
